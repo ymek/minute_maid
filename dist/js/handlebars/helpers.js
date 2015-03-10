802f689521bdb577f6f8361940b8a285a19b3688
edwinhermawan <edwin@signaturepages.com>
2015-03-09 17:56:35 -0700

    Added Dist files

---
+++ dist/js/handlebars/helpers.js
@@ -0,0 +1,72 @@
+jQuery(function($) {
+    'use strict';
+
+    //
+    // Handlebars Helpers
+    //
+
+    Handlebars.registerHelper('report-type-link', function(endpoint) {
+        var text = endpoint;
+        // NOTE: We tack 'awareness' on to make routing cleaner, and to
+        // 'automagically' load the initial data view page
+        var result = '<a href="#/' + endpoint + '/awareness">' + text.replace('-', ' ') + '</a>';
+
+        return new Handlebars.SafeString(result);
+    });
+
+    Handlebars.registerHelper('report-subnav', function(endpoint) {
+        var html = '<li>\n';
+        html += '<a href="#">' + endpoint.replace('-', ' ') + '</a>\n';
+        html += '<ul>\n';
+        $.each(
+            [
+                'awareness',
+                'consideration',
+                'brand-imagery',
+                'brand-personality',
+                'summary'
+            ],
+            function(_, value) {
+              var text = value.replace('-', ' ');
+              html += '\t<li><a href="#/' + endpoint + '/' + value + '">' + text + '</a></li>\n';
+            }
+        );
+        html += '</ul>\n';
+        html += '</li>\n';
+
+        return new Handlebars.SafeString(html);
+    });
+
+    Handlebars.registerHelper('brand-view-link', function(endpoint) {
+        var newPath = '';
+        var css = '';
+        var text = endpoint;
+        var currentRoute = window.location.hash.slice(1);
+        var baseRoute = currentRoute.slice(0, currentRoute.lastIndexOf('/'));
+
+        if (currentRoute.substring(baseRoute.length).slice(1) == endpoint) {
+            css = 'class="active"'
+        }
+        if (baseRoute.length < 2) {
+            baseRoute = currentRoute;
+        }
+        newPath = baseRoute + '/' + endpoint;
+
+        var result = '<a href="#' + newPath + '" ' + css + '>' + text.replace('-', ' ') + '</a>';
+
+        return new Handlebars.SafeString(result);
+    });
+
+    Handlebars.registerHelper('delta-view-row', function(delta) {
+      if (delta === undefined) {
+        return '<h6 class="' + cssClass + '">' + '</h6>';
+
+      } else {
+        var value = parseInt(delta['value']);
+        var cssClass = delta['change'];
+        var sign = (value > 0) ? '+' : '';
+
+        return '<h6 class="' + cssClass + '">' + sign + value + '</h6>';
+      }
+    });
+});
---
+++ dist/js/vendor/overlay.js
@@ -0,0 +1,51 @@
+jQuery(function($) {
+    'use strict';
+
+    window.Overlay = {
+        init: function() {
+            this.$triggerBttn = $('.trigger-menu');
+            this.$overlay = $('div.overlay');
+            this.$closeBttn = $(this.$overlay, 'button.overlay-close');
+            this.transEndEventNames = {
+                'WebkitTransition': 'webkitTransitionEnd',
+                'MozTransition': 'transitionend',
+                'OTransition': 'oTransitionEnd',
+                'msTransition': 'MSTransitionEnd',
+                'transition': 'transitionend'
+            };
+            this.transEndEventName = this.transEndEventNames[Modernizr.prefixed('transition')];
+            this.support = { transitions : Modernizr.csstransitions };
+
+            // TODO: Investigate whether these leave orphan event handlers/references
+            this.$triggerBttn.on('click', this.toggleOverlay);
+            this.$closeBttn.on('click', this.toggleOverlay);
+        },
+        toggleOverlay: function() {
+            // Click event handler; can safely reference window.Overlay
+            if (Overlay.$overlay.hasClass('open')) {
+                Overlay.$overlay.removeClass('open');
+                Overlay.$overlay.addClass('close');
+                var onEndTransitionFn = function(ev) {
+                    var originalEvent = ev.originalEvent;
+
+                    if (Overlay.support.transitions) {
+                        if (originalEvent.propertyName !== 'visibility') {
+                          return;
+                        }
+                        this.removeEventListener(Overlay.transEndEventName, onEndTransitionFn);
+                    }
+                    Overlay.$overlay.removeClass('close');
+                };
+                if (Overlay.support.transitions) {
+                    Overlay.$overlay.on(Overlay.transEndEventName, onEndTransitionFn);
+                }
+                else {
+                    onEndTransitionFn();
+                }
+            }
+            else if (!Overlay.$overlay.hasClass('close')) {
+                Overlay.$overlay.addClass('open');
+            }
+        }
+    };
+});
